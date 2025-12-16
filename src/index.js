6fd331f94d46df0dc301e606826169f5bccb014c
moshahidraza1 <codinghandle@gmail.com>
2025-12-16 15:24:01 +0530

    setup

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import dotenv from "dotenv";
+import {app} from "./app.js";
+dotenv.config();
+
+app.listen(process.env.PORT, ()=>{
+    console.log(`Server running on port: ${process.env.PORT}`);
+});
